813982f49b020e078752780b91d604e583d9c7e3
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-04-17 15:28:06 +0530

    Optional Exception handling refactoring

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -27,6 +27,6 @@ class ParticipateInForumTest extends TestCase
     function unauthenticated_users_may_not_add_replies()  
     {
         $this->expectException('Illuminate\Auth\AuthenticationException');
-        $this->post('threads/1/replies', []);
+        $this->withoutExceptionHandling()->post('threads/1/replies', []);
     }
 }
