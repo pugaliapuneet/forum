f8ab2280afd9abdc95500ffd7c7f72d3401e4dcc
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-04-18 12:55:44 +0530

    UI for favoriting replies

--- resources/views/thread/reply.blade.php
+++ resources/views/thread/reply.blade.php
@@ -1,9 +1,23 @@
 <div class="card">
     <div class="card-header">
-        <a href="#">
-            {{$reply->owner->name}}
-        </a> said 
-        {{$reply->created_at->diffForHumans()}}
+        <div class="level">
+            <h6 class="flex">
+                <a href="#">
+                    {{$reply->owner->name}}
+                </a> said 
+                {{$reply->created_at->diffForHumans()}}
+            </h6>
+
+            <div>
+                <form action="/replies/{{$reply->id}}/favorites" method="POST">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-light" {{$reply->isFavorited() ? 'disabled' : ''}}>
+                        {{ $reply->favorites->count() }} {{Str::plural('Favorite', $reply->favorites->count())}}
+                    </button>
+                </form>
+            </div>
+
+        </div>
     </div>
 
     <div class="card-body">
