ca49fb3b02a8444a162dae4d5a1998ab35dfa1ce
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-04-18 12:28:36 +0530

    TDD favoriting replies

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ Route::get('/threads/{channel}', 'ThreadController@index');
 
 // Route::resource('threads', 'ThreadController');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
+
+Route::post('replies/{reply}/favorites', 'FavoritesController@store');
