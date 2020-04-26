037f2f6836b3ce2a624de6f8511fc2d982a89e43
Puneet Pugalia <puneet.pugalia@gmail.com>
2020-04-26 11:27:12 +0530

    Vueside admin authorization and thread locking UI

--- resources/views/thread/show.blade.php
+++ resources/views/thread/show.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('content')
-<thread-view :initial-replies-count="{{$thread->replies_count}}" inline-template>
+<thread-view :data-replies-count="{{$thread->replies_count}}" :data-locked="{{$thread->locked}}" inline-template>
     <div class="container">
         <div class="row">
             <div class="col-md-8">
@@ -48,7 +48,8 @@
                         
                         
                         <div>
-                            <subscribe-button :active="{{json_encode($thread->isSubscribedTo)}}"></subscribe-button>
+                            <subscribe-button v-if="signedIn" :active="{{json_encode($thread->isSubscribedTo)}}"></subscribe-button>
+                            <button class="btn btn-danger" @click="locked = true" v-if="authorize('isAdmin') && !locked">Lock</button>
                         </div>
                     </div>
                 </div>
